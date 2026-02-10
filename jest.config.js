584498edeac3eb918b1090c38dd0f576c29c029b
starostin <starostinoleg370@gmail.com>
2026-02-10 16:08:59 +0000

    Clean

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
-  collectCoverageFrom: ['script.js'],
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
-};
